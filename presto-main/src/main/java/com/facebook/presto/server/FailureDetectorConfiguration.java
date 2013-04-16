prestodb/presto

Improvements to failure detector
- Remember failed services for a while after they disappear from discovery
- Use warmup period for any failed->successful transition to reduce flip-flopping around the boundary

presto-main/src/main/java/com/facebook/presto/server/FailureDetectorConfiguration.java
@@ -7,8 +7,6 @@ import io.airlift.units.MinDuration;
 
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +16,21 @@ public class FailureDetectorConfiguration
     private double failureRatioThreshold = 0.01; // 1% failure rate
     private Duration hearbeatInterval = new Duration(500, TimeUnit.MILLISECONDS);
     private Duration warmupInterval = new Duration(5, TimeUnit.SECONDS);
+    private Duration gcGraceInterval = new Duration(10, TimeUnit.MINUTES);
+
+    @NotNull
+    public Duration getGcGraceInterval()
+    {
+        return gcGraceInterval;
+    }
+
+    @Config("failure-detector.gc-grace-interval")
+    @ConfigDescription("How long to wait before 'forgetting' a service after it disappears from discovery")
+    public FailureDetectorConfiguration setGcGraceInterval(Duration gcGraceInterval)
+    {
+        this.gcGraceInterval = gcGraceInterval;
+        return this;
+    }
 
     public boolean isEnabled()
     {
@@ -38,7 +51,7 @@ public class FailureDetectorConfiguration
     }
 
     @Config("failure-detector.warmup-interval")
-    @ConfigDescription("How long to wait before considering a new node alive")
+    @ConfigDescription("How long to wait after transitioning to success before considering a service alive")
     public FailureDetectorConfiguration setWarmupInterval(Duration warmupInterval)
     {
         this.warmupInterval = warmupInterval;

presto-main/src/test/java/com/facebook/presto/server/TestFailureDetectorConfiguration.java
@@ -14,6 +14,7 @@ public class TestFailureDetectorConfiguration
     public void testDefaults()
     {
         ConfigAssertions.assertRecordedDefaults(ConfigAssertions.recordDefaults(FailureDetectorConfiguration.class)
+                .setGcGraceInterval(new Duration(10, TimeUnit.MINUTES))
                 .setFailureRatioThreshold(0.01)
                 .setHearbeatInterval(new Duration(500, TimeUnit.MILLISECONDS))
                 .setWarmupInterval(new Duration(5, TimeUnit.SECONDS))
@@ -24,6 +25,7 @@ public class TestFailureDetectorConfiguration
     public void testExplicitPropertyMappings()
     {
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("failure-detector.gc-grace-interval", "5m")
                 .put("failure-detector.warmup-interval", "60s")
                 .put("failure-detector.heartbeat-interval", "10s")
                 .put("failure-detector.threshold", "0.5")
@@ -31,6 +33,7 @@ public class TestFailureDetectorConfiguration
                 .build();
 
         FailureDetectorConfiguration expected = new FailureDetectorConfiguration()
+                .setGcGraceInterval(new Duration(5, TimeUnit.MINUTES))
                 .setWarmupInterval(new Duration(60, TimeUnit.SECONDS))
                 .setHearbeatInterval(new Duration(10, TimeUnit.SECONDS))
                 .setFailureRatioThreshold(0.5)

InsightsDev-dev/presto-1

presto-main/src/main/java/com/facebook/presto/server/FailureDetectorConfiguration.java
@@ -7,8 +7,6 @@ import io.airlift.units.MinDuration;
 
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.concurrent.TimeUnit;
 
@@ -16,8 +14,23 @@ public class FailureDetectorConfiguration
 {
     private boolean enabled = true;
     private double failureRatioThreshold = 0.01; // 1% failure rate
-    private Duration hearbeatInterval = new Duration(500, TimeUnit.MILLISECONDS);
+    private Duration heartbeatInterval = new Duration(500, TimeUnit.MILLISECONDS);
     private Duration warmupInterval = new Duration(5, TimeUnit.SECONDS);
+    private Duration expirationGraceInterval = new Duration(10, TimeUnit.MINUTES);
+
+    @NotNull
+    public Duration getExpirationGraceInterval()
+    {
+        return expirationGraceInterval;
+    }
+
+    @Config("failure-detector.expiration-grace-interval")
+    @ConfigDescription("How long to wait before 'forgetting' a service after it disappears from discovery")
+    public FailureDetectorConfiguration setExpirationGraceInterval(Duration expirationGraceInterval)
+    {
+        this.expirationGraceInterval = expirationGraceInterval;
+        return this;
+    }
 
     public boolean isEnabled()
     {
@@ -38,7 +51,7 @@ public class FailureDetectorConfiguration
     }
 
     @Config("failure-detector.warmup-interval")
-    @ConfigDescription("How long to wait before considering a new node alive")
+    @ConfigDescription("How long to wait after transitioning to success before considering a service alive")
     public FailureDetectorConfiguration setWarmupInterval(Duration warmupInterval)
     {
         this.warmupInterval = warmupInterval;
@@ -47,15 +60,15 @@ public class FailureDetectorConfiguration
 
     @MinDuration("1ms")
     @NotNull
-    public Duration getHearbeatInterval()
+    public Duration getHeartbeatInterval()
     {
-        return hearbeatInterval;
+        return heartbeatInterval;
     }
 
     @Config("failure-detector.heartbeat-interval")
-    public FailureDetectorConfiguration setHearbeatInterval(Duration interval)
+    public FailureDetectorConfiguration setHeartbeatInterval(Duration interval)
     {
-        this.hearbeatInterval = interval;
+        this.heartbeatInterval = interval;
         return this;
     }
 

presto-main/src/test/java/com/facebook/presto/server/TestFailureDetectorConfiguration.java
@@ -14,8 +14,9 @@ public class TestFailureDetectorConfiguration
     public void testDefaults()
     {
         ConfigAssertions.assertRecordedDefaults(ConfigAssertions.recordDefaults(FailureDetectorConfiguration.class)
+                .setExpirationGraceInterval(new Duration(10, TimeUnit.MINUTES))
                 .setFailureRatioThreshold(0.01)
-                .setHearbeatInterval(new Duration(500, TimeUnit.MILLISECONDS))
+                .setHeartbeatInterval(new Duration(500, TimeUnit.MILLISECONDS))
                 .setWarmupInterval(new Duration(5, TimeUnit.SECONDS))
                 .setEnabled(true));
     }
@@ -24,6 +25,7 @@ public class TestFailureDetectorConfiguration
     public void testExplicitPropertyMappings()
     {
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
+                .put("failure-detector.expiration-grace-interval", "5m")
                 .put("failure-detector.warmup-interval", "60s")
                 .put("failure-detector.heartbeat-interval", "10s")
                 .put("failure-detector.threshold", "0.5")
@@ -31,8 +33,9 @@ public class TestFailureDetectorConfiguration
                 .build();
 
         FailureDetectorConfiguration expected = new FailureDetectorConfiguration()
+                .setExpirationGraceInterval(new Duration(5, TimeUnit.MINUTES))
                 .setWarmupInterval(new Duration(60, TimeUnit.SECONDS))
-                .setHearbeatInterval(new Duration(10, TimeUnit.SECONDS))
+                .setHeartbeatInterval(new Duration(10, TimeUnit.SECONDS))
                 .setFailureRatioThreshold(0.5)
                 .setEnabled(false);
 