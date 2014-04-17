prestodb/presto

Add generic literal

presto-main/src/main/java/com/facebook/presto/sql/analyzer/ExpressionAnalyzer.java
@@ -32,6 +32,7 @@ import com.facebook.presto.sql.tree.DoubleLiteral;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.Extract;
 import com.facebook.presto.sql.tree.FunctionCall;
+import com.facebook.presto.sql.tree.GenericLiteral;
 import com.facebook.presto.sql.tree.IfExpression;
 import com.facebook.presto.sql.tree.InListExpression;
 import com.facebook.presto.sql.tree.InPredicate;
@@ -394,6 +395,26 @@ public class ExpressionAnalyzer
         {
             expressionTypes.put(node, BIGINT);
             return BIGINT;
+        }
+
+        @Override
+        protected Type visitGenericLiteral(GenericLiteral node, AnalysisContext context)
+        {
+            Type type = metadata.getType(node.getType());
+            if (type == null) {
+                throw new SemanticException(TYPE_MISMATCH, node, "Unknown type: " + node.getType());
+            }
+
+            try {
+                OperatorInfo operator = metadata.getOperator(OperatorType.CAST, type, ImmutableList.of(VARCHAR));
+                resolvedOperators.put(node, operator);
+            }
+            catch (IllegalArgumentException e) {
+                throw new SemanticException(TYPE_MISMATCH, node, "No literal form for type %s", type);
+            }
+
+            expressionTypes.put(node, type);
+            return type;
         }
 
         @Override

presto-main/src/main/java/com/facebook/presto/sql/planner/LiteralInterpreter.java
@@ -13,10 +13,15 @@
  */
 package com.facebook.presto.sql.planner;
 
+import com.facebook.presto.metadata.Metadata;
+import com.facebook.presto.metadata.OperatorInfo;
+import com.facebook.presto.metadata.OperatorInfo.OperatorType;
 import com.facebook.presto.spi.type.BigintType;
 import com.facebook.presto.spi.type.BooleanType;
 import com.facebook.presto.spi.type.DoubleType;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.analyzer.SemanticException;
+import com.facebook.presto.sql.analyzer.Session;
 import com.facebook.presto.sql.tree.AstVisitor;
 import com.facebook.presto.sql.tree.BooleanLiteral;
 import com.facebook.presto.sql.tree.Cast;
@@ -24,6 +29,7 @@ import com.facebook.presto.sql.tree.DateLiteral;
 import com.facebook.presto.sql.tree.DoubleLiteral;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.FunctionCall;
+import com.facebook.presto.sql.tree.GenericLiteral;
 import com.facebook.presto.sql.tree.IntervalLiteral;
 import com.facebook.presto.sql.tree.Literal;
 import com.facebook.presto.sql.tree.LongLiteral;
@@ -33,24 +39,27 @@ import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.sql.tree.StringLiteral;
 import com.facebook.presto.sql.tree.TimeLiteral;
 import com.facebook.presto.sql.tree.TimestampLiteral;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
 
 import java.util.List;
 
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
+import static com.facebook.presto.sql.analyzer.SemanticErrorCode.TYPE_MISMATCH;
 import static com.google.common.base.Charsets.UTF_8;
 
 public final class LiteralInterpreter
 {
     private LiteralInterpreter() {}
 
-    public static Object evaluate(Expression node)
+    public static Object evaluate(Session session, Expression node, Metadata metadata)
     {
         if (!(node instanceof Literal)) {
             throw new IllegalArgumentException("node must be a Literal");
         }
-        return new LiteralVisitor().process(node, null);
+        return new LiteralVisitor(metadata).process(node, session);
     }
 
     public static List<Expression> toExpressions(List<?> objects, List<? extends Type> types)
@@ -112,58 +121,88 @@ public final class LiteralInterpreter
     }
 
     private static class LiteralVisitor
-            extends AstVisitor<Object, Void>
+            extends AstVisitor<Object, Session>
     {
+        private final Metadata metadata;
+
+        private LiteralVisitor(Metadata metadata)
+        {
+            this.metadata = metadata;
+        }
+
         @Override
-        protected Object visitLiteral(Literal node, Void context)
+        protected Object visitLiteral(Literal node, Session session)
         {
             throw new UnsupportedOperationException("Unhandled literal type: " + node);
         }
 
         @Override
-        protected Object visitBooleanLiteral(BooleanLiteral node, Void context)
+        protected Object visitBooleanLiteral(BooleanLiteral node, Session session)
         {
             return node.getValue();
         }
 
         @Override
-        protected Long visitLongLiteral(LongLiteral node, Void context)
+        protected Long visitLongLiteral(LongLiteral node, Session session)
         {
             return node.getValue();
         }
 
         @Override
-        protected Double visitDoubleLiteral(DoubleLiteral node, Void context)
+        protected Double visitDoubleLiteral(DoubleLiteral node, Session session)
         {
             return node.getValue();
         }
 
         @Override
-        protected Slice visitStringLiteral(StringLiteral node, Void context)
+        protected Slice visitStringLiteral(StringLiteral node, Session session)
         {
             return node.getSlice();
         }
 
         @Override
-        protected Object visitDateLiteral(DateLiteral node, Void context)
+        protected Object visitDateLiteral(DateLiteral node, Session session)
         {
             return node.getUnixTime();
         }
 
         @Override
-        protected Object visitTimeLiteral(TimeLiteral node, Void context)
+        protected Object visitGenericLiteral(GenericLiteral node, Session session)
+        {
+            Type type = metadata.getType(node.getType());
+            if (type == null) {
+                throw new SemanticException(TYPE_MISMATCH, node, "Unknown type: " + node.getType());
+            }
+
+            OperatorInfo operator;
+            try {
+                operator = metadata.getOperator(OperatorType.CAST, type, ImmutableList.of(VARCHAR));
+            }
+            catch (IllegalArgumentException e) {
+                throw new SemanticException(TYPE_MISMATCH, node, "No literal form for type %s", type);
+            }
+            try {
+                return ExpressionInterpreter.invoke(session, operator.getMethodHandle(), ImmutableList.<Object>of(Slices.utf8Slice(node.getValue())));
+            }
+            catch (Throwable throwable) {
+                throw Throwables.propagate(throwable);
+            }
+        }
+
+        @Override
+        protected Object visitTimeLiteral(TimeLiteral node, Session session)
         {
             return node.getUnixTime();
         }
 
         @Override
-        protected Long visitTimestampLiteral(TimestampLiteral node, Void context)
+        protected Long visitTimestampLiteral(TimestampLiteral node, Session session)
         {
             return node.getUnixTime();
         }
 
         @Override
-        protected Long visitIntervalLiteral(IntervalLiteral node, Void context)
+        protected Long visitIntervalLiteral(IntervalLiteral node, Session session)
         {
             if (node.isYearToMonth()) {
                 throw new UnsupportedOperationException("Month based intervals not supported yet: " + node.getType());
@@ -172,7 +211,7 @@ public final class LiteralInterpreter
         }
 
         @Override
-        protected Object visitNullLiteral(NullLiteral node, Void context)
+        protected Object visitNullLiteral(NullLiteral node, Session session)
         {
             return null;
         }

InsightsDev-dev/presto-1

presto-main/src/main/java/com/facebook/presto/sql/analyzer/ExpressionAnalyzer.java
@@ -33,6 +33,7 @@ import com.facebook.presto.sql.tree.DoubleLiteral;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.Extract;
 import com.facebook.presto.sql.tree.FunctionCall;
+import com.facebook.presto.sql.tree.GenericLiteral;
 import com.facebook.presto.sql.tree.IfExpression;
 import com.facebook.presto.sql.tree.InListExpression;
 import com.facebook.presto.sql.tree.InPredicate;
@@ -394,6 +395,26 @@ public class ExpressionAnalyzer
         {
             expressionTypes.put(node, BIGINT);
             return BIGINT;
+        }
+
+        @Override
+        protected Type visitGenericLiteral(GenericLiteral node, AnalysisContext context)
+        {
+            Type type = metadata.getType(node.getType());
+            if (type == null) {
+                throw new SemanticException(TYPE_MISMATCH, node, "Unknown type: " + node.getType());
+            }
+
+            try {
+                OperatorInfo operator = metadata.getExactOperator(OperatorType.CAST, type, ImmutableList.of(VARCHAR));
+                resolvedOperators.put(node, operator);
+            }
+            catch (IllegalArgumentException e) {
+                throw new SemanticException(TYPE_MISMATCH, node, "No literal form for type %s", type);
+            }
+
+            expressionTypes.put(node, type);
+            return type;
         }
 
         @Override

presto-main/src/main/java/com/facebook/presto/sql/planner/LiteralInterpreter.java
@@ -13,7 +13,12 @@
  */
 package com.facebook.presto.sql.planner;
 
+import com.facebook.presto.metadata.Metadata;
+import com.facebook.presto.metadata.OperatorInfo;
+import com.facebook.presto.metadata.OperatorInfo.OperatorType;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.analyzer.SemanticException;
+import com.facebook.presto.sql.analyzer.Session;
 import com.facebook.presto.sql.tree.AstVisitor;
 import com.facebook.presto.sql.tree.BooleanLiteral;
 import com.facebook.presto.sql.tree.Cast;
@@ -21,6 +26,7 @@ import com.facebook.presto.sql.tree.DateLiteral;
 import com.facebook.presto.sql.tree.DoubleLiteral;
 import com.facebook.presto.sql.tree.Expression;
 import com.facebook.presto.sql.tree.FunctionCall;
+import com.facebook.presto.sql.tree.GenericLiteral;
 import com.facebook.presto.sql.tree.IntervalLiteral;
 import com.facebook.presto.sql.tree.Literal;
 import com.facebook.presto.sql.tree.LongLiteral;
@@ -30,8 +36,10 @@ import com.facebook.presto.sql.tree.QualifiedName;
 import com.facebook.presto.sql.tree.StringLiteral;
 import com.facebook.presto.sql.tree.TimeLiteral;
 import com.facebook.presto.sql.tree.TimestampLiteral;
+import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
 
 import java.util.List;
 
@@ -39,6 +47,7 @@ import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.facebook.presto.spi.type.BooleanType.BOOLEAN;
 import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
+import static com.facebook.presto.sql.analyzer.SemanticErrorCode.TYPE_MISMATCH;
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -47,12 +56,12 @@ public final class LiteralInterpreter
 {
     private LiteralInterpreter() {}
 
-    public static Object evaluate(Expression node)
+    public static Object evaluate(Session session, Expression node, Metadata metadata)
     {
         if (!(node instanceof Literal)) {
             throw new IllegalArgumentException("node must be a Literal");
         }
-        return new LiteralVisitor().process(node, null);
+        return new LiteralVisitor(metadata).process(node, session);
     }
 
     public static List<Expression> toExpressions(List<?> objects, List<? extends Type> types)
@@ -118,58 +127,88 @@ public final class LiteralInterpreter
     }
 
     private static class LiteralVisitor
-            extends AstVisitor<Object, Void>
+            extends AstVisitor<Object, Session>
     {
+        private final Metadata metadata;
+
+        private LiteralVisitor(Metadata metadata)
+        {
+            this.metadata = metadata;
+        }
+
         @Override
-        protected Object visitLiteral(Literal node, Void context)
+        protected Object visitLiteral(Literal node, Session session)
         {
             throw new UnsupportedOperationException("Unhandled literal type: " + node);
         }
 
         @Override
-        protected Object visitBooleanLiteral(BooleanLiteral node, Void context)
+        protected Object visitBooleanLiteral(BooleanLiteral node, Session session)
         {
             return node.getValue();
         }
 
         @Override
-        protected Long visitLongLiteral(LongLiteral node, Void context)
+        protected Long visitLongLiteral(LongLiteral node, Session session)
         {
             return node.getValue();
         }
 
         @Override
-        protected Double visitDoubleLiteral(DoubleLiteral node, Void context)
+        protected Double visitDoubleLiteral(DoubleLiteral node, Session session)
         {
             return node.getValue();
         }
 
         @Override
-        protected Slice visitStringLiteral(StringLiteral node, Void context)
+        protected Slice visitStringLiteral(StringLiteral node, Session session)
         {
             return node.getSlice();
         }
 
         @Override
-        protected Object visitDateLiteral(DateLiteral node, Void context)
+        protected Object visitDateLiteral(DateLiteral node, Session session)
         {
             return node.getUnixTime();
         }
 
         @Override
-        protected Object visitTimeLiteral(TimeLiteral node, Void context)
+        protected Object visitGenericLiteral(GenericLiteral node, Session session)
+        {
+            Type type = metadata.getType(node.getType());
+            if (type == null) {
+                throw new SemanticException(TYPE_MISMATCH, node, "Unknown type: " + node.getType());
+            }
+
+            OperatorInfo operator;
+            try {
+                operator = metadata.getExactOperator(OperatorType.CAST, type, ImmutableList.of(VARCHAR));
+            }
+            catch (IllegalArgumentException e) {
+                throw new SemanticException(TYPE_MISMATCH, node, "No literal form for type %s", type);
+            }
+            try {
+                return ExpressionInterpreter.invoke(session, operator.getMethodHandle(), ImmutableList.<Object>of(Slices.utf8Slice(node.getValue())));
+            }
+            catch (Throwable throwable) {
+                throw Throwables.propagate(throwable);
+            }
+        }
+
+        @Override
+        protected Object visitTimeLiteral(TimeLiteral node, Session session)
         {
             return node.getUnixTime();
         }
 
         @Override
-        protected Long visitTimestampLiteral(TimestampLiteral node, Void context)
+        protected Long visitTimestampLiteral(TimestampLiteral node, Session session)
         {
             return node.getUnixTime();
         }
 
         @Override
-        protected Long visitIntervalLiteral(IntervalLiteral node, Void context)
+        protected Long visitIntervalLiteral(IntervalLiteral node, Session session)
         {
             if (node.isYearToMonth()) {
                 throw new UnsupportedOperationException("Month based intervals not supported yet: " + node.getType());
@@ -178,7 +217,7 @@ public final class LiteralInterpreter
         }
 
         @Override
-        protected Object visitNullLiteral(NullLiteral node, Void context)
+        protected Object visitNullLiteral(NullLiteral node, Session session)
         {
             return null;
         }