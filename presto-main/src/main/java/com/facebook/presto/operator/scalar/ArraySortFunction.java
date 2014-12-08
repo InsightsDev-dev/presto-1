prestodb/presto

Make Row type comparable

presto-main/src/main/java/com/facebook/presto/operator/scalar/ArraySortFunction.java
@@ -17,9 +17,6 @@ import com.facebook.presto.metadata.FunctionInfo;
 import com.facebook.presto.metadata.ParametricScalar;
 import com.facebook.presto.metadata.Signature;
 import com.facebook.presto.spi.PrestoException;
-import com.facebook.presto.spi.block.Block;
-import com.facebook.presto.spi.block.BlockBuilder;
-import com.facebook.presto.spi.block.BlockBuilderStatus;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.TypeManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,7 +24,6 @@ import com.fasterxml.jackson.databind.type.CollectionType;
 import com.google.common.collect.ImmutableList;
 import io.airlift.json.ObjectMapperProvider;
 import io.airlift.slice.Slice;
-import io.airlift.slice.Slices;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandle;
@@ -39,6 +35,7 @@ import java.util.Map;
 import static com.facebook.presto.metadata.Signature.orderableTypeParameter;
 import static com.facebook.presto.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
 import static com.facebook.presto.type.ArrayType.toStackRepresentation;
+import static com.facebook.presto.type.TypeUtils.createBlock;
 import static com.facebook.presto.type.TypeUtils.parameterizedTypeName;
 import static com.facebook.presto.util.Reflection.methodHandle;
 import static com.google.common.base.Preconditions.checkArgument;
@@ -108,27 +105,5 @@ public final class ArraySortFunction
             }
         });
         return toStackRepresentation(elements);
-    }
-
-    private static Block createBlock(Type type, Object element)
-    {
-        BlockBuilder blockBuilder = type.createBlockBuilder(new BlockBuilderStatus());
-        Class<?> javaType = type.getJavaType();
-        if (javaType == boolean.class) {
-            type.writeBoolean(blockBuilder, (Boolean) element);
-        }
-        else if (javaType == long.class) {
-            type.writeLong(blockBuilder, ((Number) element).longValue());
-        }
-        else if (javaType == double.class) {
-            type.writeDouble(blockBuilder, (Double) element);
-        }
-        else if (javaType == Slice.class) {
-            type.writeSlice(blockBuilder, Slices.utf8Slice(element.toString()));
-        }
-        else {
-            throw new PrestoException(INVALID_FUNCTION_ARGUMENT, format("Unexpected type %s", javaType.getName()));
-        }
-        return blockBuilder.build();
     }
 }

presto-main/src/main/java/com/facebook/presto/type/RowType.java
@@ -14,6 +14,7 @@
 package com.facebook.presto.type;
 
 import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
@@ -21,23 +22,33 @@ import com.facebook.presto.spi.block.VariableWidthBlockBuilder;
 import com.facebook.presto.spi.type.AbstractVariableWidthType;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.TypeSignature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import io.airlift.json.ObjectMapperProvider;
 import io.airlift.slice.Slice;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.facebook.presto.spi.StandardErrorCode.INTERNAL_ERROR;
 import static com.facebook.presto.type.TypeJsonUtils.stackRepresentationToObject;
+import static com.facebook.presto.type.TypeUtils.createBlock;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class RowType
         extends AbstractVariableWidthType
 {
     private final List<RowField> fields;
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapperProvider().get();
+    private static final CollectionType COLLECTION_TYPE = OBJECT_MAPPER.getTypeFactory().constructCollectionType(List.class, Object.class);
 
     public RowType(List<Type> fieldTypes, List<String> fieldNames)
     {
@@ -143,6 +154,66 @@ public class RowType
         public String getName()
         {
             return name;
+        }
+    }
+
+    @Override
+    public boolean isComparable()
+    {
+        return Iterables.all(fields, new Predicate<RowField>() {
+            @Override
+            public boolean apply(RowField field)
+            {
+                return field.type.isComparable();
+            }
+        });
+    }
+
+    @Override
+    public boolean equalTo(Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
+    {
+        return deepEquals(leftBlock, leftPosition, rightBlock, rightPosition);
+    }
+
+    private boolean deepEquals(Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
+    {
+        final List<Object> leftRow = extractElements(leftBlock, leftPosition);
+        final List<Object> rightRow = extractElements(rightBlock, rightPosition);
+
+        int nFields = leftRow.size();
+        for (int i = 0; i < nFields; i++) {
+            Object leftElement = leftRow.get(i);
+            Object rightElement = rightRow.get(i);
+            Type fieldType = fields.get(i).getType();
+            if (!fieldType.equalTo(createBlock(fieldType, leftElement), 0, createBlock(fieldType, rightElement), 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hash(Block block, int position)
+    {
+        final List<Object> elements = extractElements(block, position);
+        int result = 1;
+        int nFields = elements.size();
+        for (int i = 0; i < nFields; i++) {
+            Object element = elements.get(i);
+            Type elementType = fields.get(i).getType();
+            result = 31 * result + elementType.hash(createBlock(elementType, element), 0);
+        }
+        return result;
+    }
+
+    private List<Object> extractElements(Block block, int position)
+    {
+        Slice value = block.getSlice(position, 0, block.getLength(position));
+        try {
+            return OBJECT_MAPPER.readValue(value.getBytes(), COLLECTION_TYPE);
+        }
+        catch (IOException e) {
+            throw new PrestoException(INTERNAL_ERROR, e);
         }
     }
 }

InsightsDev-dev/presto-1

presto-main/src/main/java/com/facebook/presto/operator/scalar/ArraySortFunction.java
@@ -17,9 +17,6 @@ import com.facebook.presto.metadata.FunctionInfo;
 import com.facebook.presto.metadata.ParametricScalar;
 import com.facebook.presto.metadata.Signature;
 import com.facebook.presto.spi.PrestoException;
-import com.facebook.presto.spi.block.Block;
-import com.facebook.presto.spi.block.BlockBuilder;
-import com.facebook.presto.spi.block.BlockBuilderStatus;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.TypeManager;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,7 +24,6 @@ import com.fasterxml.jackson.databind.type.CollectionType;
 import com.google.common.collect.ImmutableList;
 import io.airlift.json.ObjectMapperProvider;
 import io.airlift.slice.Slice;
-import io.airlift.slice.Slices;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandle;
@@ -39,6 +35,7 @@ import java.util.Map;
 import static com.facebook.presto.metadata.Signature.orderableTypeParameter;
 import static com.facebook.presto.spi.StandardErrorCode.INVALID_FUNCTION_ARGUMENT;
 import static com.facebook.presto.type.ArrayType.toStackRepresentation;
+import static com.facebook.presto.type.TypeJsonUtils.createBlock;
 import static com.facebook.presto.type.TypeUtils.parameterizedTypeName;
 import static com.facebook.presto.util.Reflection.methodHandle;
 import static com.google.common.base.Preconditions.checkArgument;
@@ -108,27 +105,5 @@ public final class ArraySortFunction
             }
         });
         return toStackRepresentation(elements);
-    }
-
-    private static Block createBlock(Type type, Object element)
-    {
-        BlockBuilder blockBuilder = type.createBlockBuilder(new BlockBuilderStatus());
-        Class<?> javaType = type.getJavaType();
-        if (javaType == boolean.class) {
-            type.writeBoolean(blockBuilder, (Boolean) element);
-        }
-        else if (javaType == long.class) {
-            type.writeLong(blockBuilder, ((Number) element).longValue());
-        }
-        else if (javaType == double.class) {
-            type.writeDouble(blockBuilder, (Double) element);
-        }
-        else if (javaType == Slice.class) {
-            type.writeSlice(blockBuilder, Slices.utf8Slice(element.toString()));
-        }
-        else {
-            throw new PrestoException(INVALID_FUNCTION_ARGUMENT, format("Unexpected type %s", javaType.getName()));
-        }
-        return blockBuilder.build();
     }
 }

presto-main/src/main/java/com/facebook/presto/type/RowType.java
@@ -14,6 +14,8 @@
 package com.facebook.presto.type;
 
 import com.facebook.presto.spi.ConnectorSession;
+import com.facebook.presto.spi.PrestoException;
+import com.facebook.presto.spi.StandardErrorCode;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.block.BlockBuilder;
 import com.facebook.presto.spi.block.BlockBuilderStatus;
@@ -21,23 +23,34 @@ import com.facebook.presto.spi.block.VariableWidthBlockBuilder;
 import com.facebook.presto.spi.type.AbstractVariableWidthType;
 import com.facebook.presto.spi.type.Type;
 import com.facebook.presto.spi.type.TypeSignature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
+import io.airlift.json.ObjectMapperProvider;
 import io.airlift.slice.Slice;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.facebook.presto.spi.StandardErrorCode.INTERNAL_ERROR;
+import static com.facebook.presto.type.TypeJsonUtils.createBlock;
 import static com.facebook.presto.type.TypeJsonUtils.stackRepresentationToObject;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.String.format;
 
 public class RowType
         extends AbstractVariableWidthType
 {
     private final List<RowField> fields;
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapperProvider().get();
+    private static final CollectionType COLLECTION_TYPE = OBJECT_MAPPER.getTypeFactory().constructCollectionType(List.class, Object.class);
 
     public RowType(List<Type> fieldTypes, List<String> fieldNames)
     {
@@ -143,6 +156,71 @@ public class RowType
         public String getName()
         {
             return name;
+        }
+    }
+
+    @Override
+    public boolean isComparable()
+    {
+        return Iterables.all(fields, new Predicate<RowField>() {
+            @Override
+            public boolean apply(RowField field)
+            {
+                return field.getType().isComparable();
+            }
+        });
+    }
+
+    @Override
+    public boolean equalTo(Block leftBlock, int leftPosition, Block rightBlock, int rightPosition)
+    {
+        List<Object> leftRow = extractElements(leftBlock, leftPosition);
+        List<Object> rightRow = extractElements(rightBlock, rightPosition);
+
+        int nFields = leftRow.size();
+        for (int i = 0; i < nFields; i++) {
+            Object leftElement = leftRow.get(i);
+            Object rightElement = rightRow.get(i);
+            checkElementNotNull(leftElement);
+            checkElementNotNull(rightElement);
+            Type fieldType = fields.get(i).getType();
+            if (!fieldType.equalTo(createBlock(fieldType, leftElement), 0, createBlock(fieldType, rightElement), 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hash(Block block, int position)
+    {
+        List<Object> elements = extractElements(block, position);
+        int result = 1;
+        int nFields = elements.size();
+        for (int i = 0; i < nFields; i++) {
+            Object element = elements.get(i);
+            checkElementNotNull(element);
+            Type elementType = fields.get(i).getType();
+            result = 31 * result + elementType.hash(createBlock(elementType, element), 0);
+        }
+        return result;
+    }
+
+    private static void checkElementNotNull(Object element)
+    {
+        if (element == null) {
+            throw new PrestoException(StandardErrorCode.NOT_SUPPORTED, "ROW comparison not supported for rows with null elements");
+        }
+    }
+
+    private List<Object> extractElements(Block block, int position)
+    {
+        Slice value = getSlice(block, position);
+        try {
+            return OBJECT_MAPPER.readValue(value.getBytes(), COLLECTION_TYPE);
+        }
+        catch (IOException e) {
+            throw new PrestoException(INTERNAL_ERROR, format("Bad native value, '%s'", value.toStringUtf8()), e);
         }
     }
 }