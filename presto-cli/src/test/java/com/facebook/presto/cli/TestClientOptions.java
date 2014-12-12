prestodb/presto

Fix behavior for USE catalog/schema

presto-cli/src/test/java/com/facebook/presto/cli/TestClientOptions.java
@@ -140,9 +140,11 @@ public class TestClientOptions
         ClientOptions options = new ClientOptions();
         ClientSession session = options.toClientSession();
         SqlParser sqlParser = new SqlParser();
-        session = Console.processSessionParameterChange(sqlParser.createStatement("USE CATALOG test_catalog"), session);
+        session = Console.processSessionParameterChange(sqlParser.createStatement("USE test_catalog.test_schema"), session);
         assertEquals(session.getCatalog(), "test_catalog");
-        session = Console.processSessionParameterChange(sqlParser.createStatement("USE SCHEMA test_schema"), session);
         assertEquals(session.getSchema(), "test_schema");
+        session = Console.processSessionParameterChange(sqlParser.createStatement("USE test_catalog.test_schema2"), session);
+        assertEquals(session.getCatalog(), "test_catalog");
+        assertEquals(session.getSchema(), "test_schema2");
     }
 }

presto-parser/src/main/antlr3/com/facebook/presto/sql/parser/StatementBuilder.g
@@ -66,7 +66,7 @@ statement returns [Statement value]
     | showColumns               { $value = $showColumns.value; }
     | showPartitions            { $value = $showPartitions.value; }
     | showFunctions             { $value = $showFunctions.value; }
-    | useCollection             { $value = $useCollection.value; }
+    | use                       { $value = $use.value; }
     | createTable               { $value = $createTable.value; }
     | dropTable                 { $value = $dropTable.value; }
     | renameTable               { $value = $renameTable.value; }
@@ -544,9 +544,9 @@ showFunctions returns [Statement value]
     : SHOW_FUNCTIONS { $value = new ShowFunctions(); }
     ;
 
-useCollection returns [Statement value]
-    : ^(USE_CATALOG ident) { $value = new UseCollection($ident.value, UseCollection.CollectionType.CATALOG); }
-    | ^(USE_SCHEMA ident) { $value = new UseCollection($ident.value, UseCollection.CollectionType.SCHEMA); }
+use returns [Statement value]
+    : ^(USE s=ident) { $value = new Use(Optional.empty(), $s.value); }
+    | ^(USE c=ident s=ident) { $value = new Use(Optional.ofNullable($c.value), $s.value); }
     ;
 
 createTable returns [Statement value]

InsightsDev-dev/presto-1

presto-cli/src/test/java/com/facebook/presto/cli/TestClientOptions.java
@@ -140,9 +140,17 @@ public class TestClientOptions
         ClientOptions options = new ClientOptions();
         ClientSession session = options.toClientSession();
         SqlParser sqlParser = new SqlParser();
-        session = Console.processSessionParameterChange(sqlParser.createStatement("USE CATALOG test_catalog"), session);
+
+        session = Console.processSessionParameterChange(sqlParser.createStatement("USE test_catalog.test_schema"), session);
         assertEquals(session.getCatalog(), "test_catalog");
-        session = Console.processSessionParameterChange(sqlParser.createStatement("USE SCHEMA test_schema"), session);
+        assertEquals(session.getSchema(), "test_schema");
+
+        session = Console.processSessionParameterChange(sqlParser.createStatement("USE test_schema_b"), session);
+        assertEquals(session.getCatalog(), "test_catalog");
+        assertEquals(session.getSchema(), "test_schema_b");
+
+        session = Console.processSessionParameterChange(sqlParser.createStatement("USE test_catalog_2.test_schema"), session);
+        assertEquals(session.getCatalog(), "test_catalog_2");
         assertEquals(session.getSchema(), "test_schema");
     }
 }

presto-parser/src/main/antlr3/com/facebook/presto/sql/parser/StatementBuilder.g
@@ -66,7 +66,7 @@ statement returns [Statement value]
     | showColumns               { $value = $showColumns.value; }
     | showPartitions            { $value = $showPartitions.value; }
     | showFunctions             { $value = $showFunctions.value; }
-    | useCollection             { $value = $useCollection.value; }
+    | use                       { $value = $use.value; }
     | createTable               { $value = $createTable.value; }
     | dropTable                 { $value = $dropTable.value; }
     | renameTable               { $value = $renameTable.value; }
@@ -544,9 +544,9 @@ showFunctions returns [Statement value]
     : SHOW_FUNCTIONS { $value = new ShowFunctions(); }
     ;
 
-useCollection returns [Statement value]
-    : ^(USE_CATALOG ident) { $value = new UseCollection($ident.value, UseCollection.CollectionType.CATALOG); }
-    | ^(USE_SCHEMA ident) { $value = new UseCollection($ident.value, UseCollection.CollectionType.SCHEMA); }
+use returns [Statement value]
+    : ^(USE schema=ident)               { $value = new Use(Optional.empty(), $schema.value); }
+    | ^(USE catalog=ident schema=ident) { $value = new Use(Optional.ofNullable($catalog.value), $schema.value); }
     ;
 
 createTable returns [Statement value]